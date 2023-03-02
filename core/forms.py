c22bcca01e710be24c71a1ad712e2415da8f2516
frederico-rufino <frederico.diniz.rufino@gmail.com>
2023-03-02 13:57:56 -0300

    Creating forms to Categoria, Veiculo e fabricante

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 from core.models import Fabricante, Categoria, Veiculo, VeiculoImage
+       
 
+class FormFabricante(forms.ModelForm):
+    class Meta:
+        model = Fabricante
+        fields = '__all__'
+
+class FormCategoria(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class FormVeiculo(forms.ModelForm):
+    class Meta:
+        model = Veiculo
+        fields = '__all__'
               
-
 class VeiculoImageForm(forms.ModelForm):
     class Meta:
         model = VeiculoImage
@@ -11,4 +25,3 @@ class VeiculoImageForm(forms.ModelForm):
         widgets = {
             'foto': forms.ClearableFileInput(attrs={'multiple': True}),
         }
-
